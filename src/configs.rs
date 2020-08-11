7ce5630b709a1250653691a78e622fa2edd6dff3
andersonjuan <andersonjuanc@gmail.com>
2020-08-10 23:10:21 -0700

    cleaned up dependencies, some compiler error fixes

--- src/configs.rs
+++ src/configs.rs
@@ -1,48 +1,49 @@
-use std::time::{Duration, SystemTime};
-
 /// Configs stores the location and m
-mod configs {
-    pub struct Configs {
-        pub path: td::path::PathBuf,
-        last_modified: SystemTime,
-        configs: td::path::PathBuf,
-        init: Boolean,
-    }
+use std::path::PathBuf;
+use std::time::SystemTime;
 
-    impl Configs {
-        pub fn init(path: td::path::PathBuf, configs: td::path::PathBuf) {
-            Configs {
-                path: path,
-                last_modified: (SystemTime::now()),
-                configs: config,
-                init: true,
-            }
+pub struct Configs<'a> {
+    pub path: &'a PathBuf,
+    last_modified: SystemTime,
+    configs: &'a PathBuf,
+    init: bool,
+}
+
+pub fn init<'a>(path: PathBuf, configs: PathBuf) -> Configs<'a> {
+    Configs {
+        path: &path,
+        last_modified: (SystemTime::now()),
+        configs: &configs,
+        init: true,
+    }
+}
+
+impl Configs<'_> {
+    pub fn update(&self, modified: SystemTime) -> bool {
+        if self.init {
+            self.last_modified = modified;
+            self.init = false;
+            return true;
         }
 
-        pub fn update(&self, modified: SystemTime) -> Boolean  {
-            if (self.init) {
-                self.last_modified = modified;
-                self.init = false;
-                return true;
-            }
-
-            if (update_needed(modified, self.last_modified)) {
-                self.last_modified = modified;
-                self.save_changes()
-                return true;
-            }
-            false;
+        if update_needed(modified, self.last_modified) {
+            self.last_modified = modified;
+            self.save_changes();
+            return true;
         }
+        false
+    }
 
-        fn save_changes(&self) {
+    fn save_changes(&self) {}
+}
 
+fn update_needed(new: SystemTime, curr: SystemTime) -> bool {
+    match new.duration_since(curr) {
+        Ok(_) => {
+            return true;
         }
-    }
-    
-    fn update_needed(new: SystemTime, curr: SystemTime) -> Boolean {
-        match (new.duration_since(curr)) {
-            Ok(_) => { true },
-            Err(_) => { false },
-        };
-    }
+        Err(_) => {
+            return false;
+        }
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,43 @@
+//use std::env;
+use std::fs;
+use std::path::PathBuf;
 use structopt::StructOpt;
+//use std::io;
+//use std::path::Path;
+//use std::ffi::OsStr;
+
+mod configs;
 
 /// target file to watch and type of operation to perform
 #[derive(Debug, StructOpt)]
 struct Cli {
     /// file to watch
     #[structopt(parse(from_os_str))]
-    path: td::path::PathBuf,
+    path: std::path::PathBuf,
     /// upload or download flag
     #[structopt(short = "u", long = "upload")]
-    upload: Boolean,
+    upload: bool,
 }
 
 /// constants for saving metadata
-const KEEPSYNCED_CONFIG_DIR: &str = ""
-
+const KEEPSYNCED_CONFIG_DIR: PathBuf = PathBuf::new();
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::from_args();
 
-    let mut configs = configs::init(&args.path);
+    let mut configs = configs::init(args.path, KEEPSYNCED_CONFIG_DIR);
 
     let metadata = fs::metadata(&args.path)?;
     let time = match metadata.modified() {
-        Ok(time) => { time },
-        Err(error) => { return Err(error.into()); }
+        Ok(time) => time,
+        Err(error) => {
+            return Err(error.into());
+        }
     };
 
-    configs.update(time)
+    configs.update(time);
 
     // TODO: complete the upload logic
 
-    Ok(());
+    Ok(())
 }
